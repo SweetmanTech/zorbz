88e957ac026053ee9c7adfc09b6ac706becdd3d2
Sweets Sweetman <sweetmantech@gmail.com>
2022-06-11 15:43:41 -0300

    I remove unused sketch code.

--- src/components/Sketch/Sketch.js
+++ src/components/Sketch/Sketch.js
@@ -3,8 +3,6 @@ import BaseSketch from 'react-p5'
 
 const windowWidth = 500
 const windowHeight = 500
-let x = 50
-let y = 50
 const Sketch = props => {
 	const [t, setT] = useState(0)
 	const [zorb, setZorb] = useState()
@@ -12,34 +10,21 @@ const Sketch = props => {
 	const setup = (p5, canvasParentRef) => {
 		// use parent to render the canvas in this ref
 		// (without that p5 will render the canvas outside of your component)
-
-		const canvas = p5.createCanvas(windowWidth, windowHeight).parent(canvasParentRef)
-		const slider = p5.createSlider(1, 10, 5)
-		//slider.position(0, h+2);
-		slider.style('width', '80px')
-		const slider2 = p5.createSlider(1, 16, 8)
-		//slider2.position(0, h+20);
-		slider2.style('width', '80px')
+		p5.createCanvas(windowWidth, windowHeight).parent(canvasParentRef)
 	}
 
 	const draw = p5 => {
-		// p5.background(0)
-		// p5.ellipse(x, y, 70, 70)
-		// // NOTE: Do not use setState in the draw function or in functions that are executed
-		// // in the draw function...
-		// // please use normal variables or class properties for these purposes
-		// x++
 		const steps = 0.005
 		setT((t += steps))
-		const fluid = 0.01 //slider.value()*fx/5;
-		const r = 24 //slider2.value();
+		const fluid = 0.01
+		const r = 24
 		const w = 102
 		const h = 102
 		const mvx = 20
 		const mvy = 20
 		p5.background(0, w)
-		for (x = 0; x < windowWidth; x += 25)
-			for (y = 0; y < windowHeight; y += 20) {
+		for (let x = 0; x < windowWidth; x += 25)
+			for (let y = 0; y < windowHeight; y += 20) {
 				const n = _ => {
 					return p5.TAU * (t + p5.sin(p5.TAU * t - p5.dist(x, y, w / 2, h / 2) * fluid))
 				}
