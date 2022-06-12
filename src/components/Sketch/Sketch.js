055c923f47cbd984c9101648212a9791d87e09c9
Wayne Hoover <w@waynehoover.com>
2022-06-12 16:37:08 -0500

    try and update with new p5

--- src/components/Sketch/Sketch.js
+++ src/components/Sketch/Sketch.js
@@ -3,8 +3,8 @@ import BaseSketch from 'react-p5'
 
 const windowWidth = 500
 const windowHeight = 500
-const NUMBER_OF_COLUMNS = 20
-const NUMBER_OF_ROWS = 25
+const NUMBER_OF_COLUMNS = 20 // 500?
+const NUMBER_OF_ROWS = 25 // 500?
 const Sketch = ({ zorbs, zoraEvents = { length: 500 } }) => {
 	const [t, setT] = useState(0)
 	const [zorbArray, setZorbArray] = useState([])
@@ -18,23 +18,25 @@ const Sketch = ({ zorbs, zoraEvents = { length: 500 } }) => {
 	const draw = p5 => {
 		const steps = 0.005
 		setT((t += steps))
-		const fluid = 0.01
-		const r = 24
-		const w = 102
-		const h = 102
+		const fluid = 0.009
+		const r = 25
+		const w = 99
 		const mvx = 20
-		const mvy = 20
+		const mvy = 5
+		let eventVar1 = 1
+		let eventVar2 = 1
+
 		p5.background(0, w)
-		for (let x = 0; x < NUMBER_OF_COLUMNS; x += 1)
-			for (let y = 0; y < NUMBER_OF_ROWS; y += 1) {
+		for (let x = 0; x < NUMBER_OF_COLUMNS; x += eventVar1)
+			for (let y = 0; y < NUMBER_OF_ROWS; y += eventVar2) {
 				const n = _ => {
-					return p5.TAU * (t + p5.sin(p5.TAU * t - p5.dist(25 * x, 20 * y, w / 2, h / 2) * fluid))
+					return p5.TAU * (t + p5.sin(p5.TAU * t - p5.dist(x, y, 250,250) * fluid))
 				}
-				const ox = 25 * x + mvx * p5.sin(n())
-				const oy = 20 * y + mvy * p5.cos(n())
+				const ox = x + mvx * p5.sin(n())
+				const oy = y + mvy * p5.cos(n())
 
 				let nz = 100
-				nz = p5.noise(25 * x * fluid, 20 * y * fluid)
+				nz = p5.noise(x * fluid, y * fluid);
 				if ((x + 1) * (y + 1) - 1 < zoraEvents.length) {
 					p5.image(zorbArray[(x + 1) * (y + 1) - 1], ox, oy, r, r)
 				}
