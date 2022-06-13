08f892ac0bc959efed653f4e2a6290cd6c7130d8
Sweets Sweetman <sweetmantech@gmail.com>
2022-06-12 22:19:01 -0300

    I update sketch to remove empty space.

--- src/components/Sketch/Sketch.js
+++ src/components/Sketch/Sketch.js
@@ -1,13 +1,16 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import BaseSketch from 'react-p5'
 
 const windowWidth = 500
 const windowHeight = 500
-const NUMBER_OF_COLUMNS = 20
-const NUMBER_OF_ROWS = 25
 const Sketch = ({ zorbs, zoraEvents = { length: 500 } }) => {
 	const [t, setT] = useState(0)
 	const [zorbArray, setZorbArray] = useState([])
+
+	useEffect(() => {
+		console.log('NUMBER OF EVENT', zoraEvents.length)
+		console.log('proportion x:y', 20 / 25)
+	}, [])
 
 	const setup = (p5, canvasParentRef) => {
 		// use parent to render the canvas in this ref
@@ -19,31 +22,36 @@ const Sketch = ({ zorbs, zoraEvents = { length: 500 } }) => {
 		const steps = 0.005
 		setT((t += steps))
 		const fluid = 0.01
-		const r = 24
-		const w = 102
-		const h = 102
+		const zorbRadius = 24
+		const w = 250
+		const h = 250
 		const mvx = 20
 		const mvy = 20
 		p5.background(0, w)
+		const NUMBER_OF_ROWS = Math.floor(Math.sqrt((5 * zoraEvents.length) / 4))
+		const NUMBER_OF_COLUMNS = Math.ceil(zoraEvents.length / NUMBER_OF_ROWS) // 20
+
 		for (let x = 0; x < NUMBER_OF_COLUMNS; x += 1)
 			for (let y = 0; y < NUMBER_OF_ROWS; y += 1) {
 				const n = _ => {
 					return p5.TAU * (t + p5.sin(p5.TAU * t - p5.dist(25 * x, 20 * y, w / 2, h / 2) * fluid))
 				}
-				const ox = 25 * x + mvx * p5.sin(n())
-				const oy = 20 * y + mvy * p5.cos(n())
+				const xCoordinate = (windowWidth / NUMBER_OF_COLUMNS) * x + mvx * p5.sin(n())
+				const yCoordinate = (windowHeight / NUMBER_OF_ROWS) * y + mvy * p5.cos(n())
 
 				let nz = 100
 				nz = p5.noise(25 * x * fluid, 20 * y * fluid)
-				if ((x + 1) * (y + 1) - 1 < zoraEvents.length) {
-					p5.image(zorbArray[(x + 1) * (y + 1) - 1], ox, oy, r, r)
+				const zorbIndex = (x + 1) * (y + 1) - 1
+				if (zorbIndex < zoraEvents.length) {
+					p5.image(zorbArray[zorbIndex], xCoordinate, yCoordinate, zorbRadius, zorbRadius)
 				}
 			}
 	}
 
 	const preload = p5 => {
 		const myImages = []
-		for (let i = 0; i < NUMBER_OF_COLUMNS * NUMBER_OF_ROWS; i++) {
+
+		for (let i = 0; i < zorbs.length; i++) {
 			myImages.push(p5.loadImage(zorbs[i]))
 		}
 		setZorbArray(myImages)
