01615f4e378726e02f06541491d7ab23068cefd2
Sweets Sweetman <sweetmantech@gmail.com>
2022-06-13 01:03:39 -0300

    I show owner (even if wallet isn't connected.

--- src/components/OwnerSection/OwnerSection.js
+++ src/components/OwnerSection/OwnerSection.js
@@ -1,29 +1,25 @@
-import { useEffect, useState } from 'react'
-import { etherscanBlockExplorers, useContract, useEnsName, useNetwork, useSigner } from 'wagmi'
+import { etherscanBlockExplorers, useContract, useContractRead, useSigner } from 'wagmi'
 import truncate from '../../utils/truncate'
 import MintButton from '../MintButton'
 import abi from './abi.json'
 
 const OwnerSection = ({ tokenId }) => {
-	const [owner, setOwner] = useState(false)
 	const { data: signer } = useSigner()
-	const { data: ens } = useEnsName({
-		address: owner,
-		staleTime: 2_000,
-	})
-	const { chains } = useNetwork()
+	const { data: owner } = useContractRead(
+		{
+			addressOrName: '0x9598be1c138350d70322613c3d7899c8f0b2b432',
+			contractInterface: abi,
+		},
+		'ownerOf',
+		{
+			args: tokenId,
+		}
+	)
 
 	const contract = useContract({
 		addressOrName: '0x9598be1c138350d70322613c3d7899c8f0b2b432',
 		contractInterface: abi,
 		signerOrProvider: signer,
-	})
-
-	useEffect(() => {
-		contract
-			.ownerOf(tokenId)
-			.then(owner => setOwner(owner))
-			.catch(console.error)
 	})
 
 	return (
@@ -32,9 +28,7 @@ const OwnerSection = ({ tokenId }) => {
 				<div className="flex justify-center pt-8 sm:justify-start sm:pt-0">
 					<a
 						target="__blank"
-						href={`${etherscanBlockExplorers[chains[0].name.toLowerCase()].url}/token/${
-							contract.address
-						}?a=${tokenId}`}
+						href={`${etherscanBlockExplorers.rinkeby?.url}/token/${contract.address}?a=${tokenId}`}
 					>
 						<h3 className="font-bold dark:text-white">Owned by: {truncate(owner)}</h3>
 					</a>
